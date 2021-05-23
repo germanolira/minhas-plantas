1f352b9fbc9ffd6ecff1c10364cca4379ebff7bf
Germano Lira <dolan.3@hotmail.com>
2021-05-23 14:04:43 -0300

    testando a api com uma foto do elon musk

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/pablojr17/plantmanager'
+  baseURL: 'https://my-json-server.typicode.com/germanolira/minhas-plantas'
 });
 
 export default api;
